e3be8f9a92554cc61f3409f15431c38db5ca8479
Alexey <enslaver@list.ru>
2020-12-13 21:01:02 +0300

    started quba.platform project

---
+++ modules/core/test/ru/expoforum/carservicecenter/CarservicecenterTestContainer.java
@@ -0,0 +1,57 @@
+package ru.expoforum.carservicecenter;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CarservicecenterTestContainer extends TestContainer {
+
+    public CarservicecenterTestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = new ArrayList<>(Arrays.asList(
+                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
+                "com.haulmont.cuba"
+        ));
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "ru/expoforum/carservicecenter/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "ru/expoforum/carservicecenter/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+
+    public static class Common extends CarservicecenterTestContainer {
+
+        public static final CarservicecenterTestContainer.Common INSTANCE = new CarservicecenterTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+
+    }
+}
