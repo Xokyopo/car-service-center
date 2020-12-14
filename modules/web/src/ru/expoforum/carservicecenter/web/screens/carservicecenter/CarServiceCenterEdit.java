52828102601857036583e17fc84c78df0f450600
Alexey <enslaver@list.ru>
2020-12-14 16:04:29 +0300

    updated set default City when create new entity

--- modules/web/src/ru/expoforum/carservicecenter/web/screens/carservicecenter/CarServiceCenterEdit.java
+++ modules/web/src/ru/expoforum/carservicecenter/web/screens/carservicecenter/CarServiceCenterEdit.java
@@ -2,10 +2,24 @@ package ru.expoforum.carservicecenter.web.screens.carservicecenter;
 
 import com.haulmont.cuba.gui.screen.*;
 import ru.expoforum.carservicecenter.entity.CarServiceCenter;
+import ru.expoforum.carservicecenter.entity.City;
+import ru.expoforum.carservicecenter.service.CityService;
+
+import javax.inject.Inject;
 
 @UiController("carservicecenter_CarServiceCenter.edit")
 @UiDescriptor("car-service-center-edit.xml")
 @EditedEntityContainer("carServiceCenterDc")
 @LoadDataBeforeShow
 public class CarServiceCenterEdit extends StandardEditor<CarServiceCenter> {
+    @Inject
+    private CityService cityService;
+
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<CarServiceCenter> event) {
+        City city = cityService.getDefaultCity();
+        if (city != null) {
+            event.getEntity().setCity(city);
+        }
+    }
 }
